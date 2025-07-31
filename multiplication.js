4635baa0516219c2479cb5481156f049b2e31a17
rezwana028 <rezwanarahman028@gmail.com>
2025-07-31 15:04:17 +0600

    multiplication table of 9 completed

---
+++ multiplication.js
@@ -0,0 +1,19 @@
+/***
+Generate a multiplication table for number 9
+ */
+/*programming hero*/
+
+// system-01
+
+// for (let i = 9; i <= 90; i += 9) {
+//     console.log(`multiplication of 9 :`, i);
+
+// }
+
+// system-02
+let number = 9;
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} * ${i} = `, number * i);
+
+}
+
