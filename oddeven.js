b11600545f49f087f874a1778e3dbad1f40b6327
rezwana028 <rezwanarahman028@gmail.com>
2025-07-31 14:27:13 +0600

    odd even task completed

---
+++ oddeven.js
@@ -0,0 +1,25 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+for (let i = 61; i <= 100; i += 2) {
+    console.log(`odd number:`, i);
+
+}
+
+
+
+/***
+Subtask-2:
+Find all the even numbers from 78 to 98.
+ */
+/*programming hero*/
+for (let i = 78; i <= 98; i++)
+    if (i % 2 === 0) {
+        console.log(`even number:`, i);
+
+    }
